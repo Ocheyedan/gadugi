bd1d83090c2aee8e3c76a925b337db5614142c46
blangel <langelb@gmail.com>
2012-03-02 16:30:35 -0500

    * Initial project structure.

---
+++ src/main/java/net/ocheyedan/gadugi/Gadugi.java
@@ -0,0 +1,15 @@
+package net.ocheyedan.gadugi;
+
+/**
+ * Date: 3/2/12
+ * Time: 4:17 PM
+ * @author Trevor Smith
+ * @author Brian Langel
+ *
+ *
+ */
+public final class Gadugi {
+
+    private Gadugi() { }
+
+}
---
+++ src/test/java/net/ocheyedan/gadugi/GadugiTest.java
@@ -0,0 +1,14 @@
+package net.ocheyedan.gadugi;
+
+import org.junit.Test;
+
+/**
+ * Date: 3/2/12
+ * Time: 4:17 PM
+ * @author Trevor Smith
+ * @author Brian Langel
+ */
+public class GadugiTest {
+
+
+}
