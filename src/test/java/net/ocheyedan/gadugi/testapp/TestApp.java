92ca945ac26e7e99fd2b9cbe692e6c09188024d1
blangel <langelb@gmail.com>
2012-03-08 20:45:30 -0500

    * Preparing for new TestApp within the test directory. * TODO - need to add two dependencies and test via Gadugi.

---
+++ src/test/java/net/ocheyedan/gadugi/testapp/TestApp.java
@@ -0,0 +1,20 @@
+package net.ocheyedan.gadugi.testapp;
+
+/**
+ * Date: 3/8/12
+ * Time: 1:58 PM
+ * @author Trevor Smith
+ * @author Brian Langel
+ * 
+ * A test client to be invoked and run by a unit test to ensure {@literal Gadugi} functions within an application.
+ */
+public class TestApp {
+    
+    public static void main(String[] args) {
+
+        // assert that ClassNotFoundException if attempt without calling Gadugi.using()
+
+
+    }
+    
+}
